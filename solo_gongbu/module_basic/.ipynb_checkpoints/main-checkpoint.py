4f60d758a1973ee99c9bbb8febf8810854de6be6
jammin <imyourdata@soongsil.ac.kr>
2025-03-04 16:54:45 +0900

    5일차: 모듈(ㅇ모듈 만들기는 환경 문제로 추후 진행)

---
+++ solo_gongbu/module_basic/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,4 @@
+# test_module 파일 가져오기
+import test_module as test
+# radius = test
+print(test.get_circumference(radius))
---
+++ solo_gongbu/module_basic/.ipynb_checkpoints/test_module-checkpoint.py
@@ -0,0 +1,11 @@
+PI = 3.141592
+
+def number_input():
+    output = input("숫자 입력> ")
+    return float(output)
+
+def get_circumference(radius):
+    return 2 * PI * radius
+
+def get_circle_aea(radius):
+    return PI * radius * radius
